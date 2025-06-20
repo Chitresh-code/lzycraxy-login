faa9675185deae1ae776ab855a7e2f2af20f4e9e
shallyJha <shallyjha855@gmail.com>
2025-06-20 15:14:39 +0530

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'LzyCrazy - Login',
+  description: 'Created with Love by LzyCrazy',
+  generator: 'Next.js',
+  applicationName: 'LzyCrazy',
+  keywords: ['LzyCrazy', 'Login', 'Next.js'],
 }
 
 export default function RootLayout({
